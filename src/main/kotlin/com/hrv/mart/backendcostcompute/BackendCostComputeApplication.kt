4879ce53ddee599312b8a8f83961f4ed968f99fd
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-03-18 12:45:58 +0530

    First Commit

---
+++ src/main/kotlin/com/hrv/mart/backendcostcompute/BackendCostComputeApplication.kt
@@ -0,0 +1,11 @@
+package com.hrv.mart.backendcostcompute
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendCostComputeApplication
+
+fun main(args: Array<String>) {
+    runApplication<BackendCostComputeApplication>(*args)
+}
