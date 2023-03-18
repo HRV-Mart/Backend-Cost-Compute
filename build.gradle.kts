036ab90233740e3a024e1baae311fb36a2aa2c7a
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-03-18 15:34:53 +0530

    Added detekt and jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.0.4"
     id("io.spring.dependency-management") version "1.1.0"
+    id("io.gitlab.arturbosch.detekt") version "1.22.0"
+    id("jacoco")// This is to use Jacoco for coverage testing
     kotlin("jvm") version "1.7.22"
     kotlin("plugin.spring") version "1.7.22"
 }
@@ -23,6 +25,12 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
+    // Detekt Formatting
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.22.0")
+}
+detekt {
+    toolVersion = "1.22.0"
+    config = files("config/detekt/detekt.yml")
 }
 
 tasks.withType<KotlinCompile> {
@@ -34,4 +42,24 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+    // To run Jacoco Test Coverage Verification
+    finalizedBy("jacocoTestCoverageVerification")
+}
+tasks.jacocoTestCoverageVerification {
+    violationRules {
+        rule {
+            excludes = listOf(
+                ""
+            )
+            limit {
+                minimum = "0.9".toBigDecimal()
+            }
+        }
+    }
+}
+tasks.jacocoTestReport{
+    reports {
+        html.required.set(true)
+        generate()
+    }
 }
